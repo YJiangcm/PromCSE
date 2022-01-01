ea91ccdfeb18ff50a29b0d924a65b3f4e88e9641
Jiang Yuxin 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2022-01-01 22:35:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,8 +34,8 @@ from transformers.tokenization_utils_base import BatchEncoding, PaddingStrategy,
 from transformers.trainer_utils import is_main_process
 from transformers.data.data_collator import DataCollatorForLanguageModeling
 from transformers.file_utils import cached_property, torch_required, is_torch_available, is_torch_tpu_available
-from simcse.models import RobertaForCL, BertForCL
-from simcse.trainers import CLTrainer
+from dcpcse.models import RobertaForCL, BertForCL
+from dcpcse.trainers import CLTrainer
 
 logger = logging.getLogger(__name__)
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_MASKED_LM_MAPPING.keys())
